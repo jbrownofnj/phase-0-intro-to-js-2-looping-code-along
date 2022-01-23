eb4630d0b3e18a417d3c72b867a942afd79356cb
John Brown <johnbrownsinbox@gmail.com>
2022-01-23 17:40:03 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameArray,giftType){
+    const cardsArray = [];
+    for (let i=0; i<nameArray.length; i++){
+        cardsArray.push("Thank you, "+nameArray[i]+", for the wonderful "+giftType+" gift!");
+}
+    return cardsArray;
+}
+
+function countDown(startingNumber){
+    for(let i=startingNumber; i>=0; i--){
+        console.log(i);
+    }
+//I see the danger of the starting number being a negative but since I know the input range (10 only) from indexTest.js I figured this would be find. 
+}
